11466b2376eb483906434a989fe76208fb10f59b
zhurof <zhuroff92@gmail.com>
2020-03-04 16:41:14 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,127 @@
+	<div class="contacts-block">
+		<div class="wrapper">
+			<div class="box contacts-block__box">
+				
+			</div>
+		</div>
+	</div>
+	<footer class="footer">
+		<div class="wrapper">
+			<div class="footer__topline">
+				<div class="footer__main">
+					<div class="menu footer__menu">
+						<div class="menu__column">
+							<ul class="menu__item">
+								<li>
+									<a href="#">Отпугиватели</a>
+									<ul>
+										<li>
+											<a href="#">Грызунов</a>
+											<ul>
+												<li><a href="#">Ястреб</a></li>
+												<li><a href="#">Торнадо</a></li>
+												<li><a href="#">Ультразвуковые</a></li>
+												<li><a href="#">Экоснайпер</a></li>
+											</ul>
+										</li>											
+										<li><a href="#">Змей</a></li>
+										<li><a href="#">Клопов</a></li>
+										<li><a href="#">Комаров</a></li>
+										<li><a href="#">Кротов</a></li>
+										<li><a href="#">Летучих мышей</a></li>
+										<li>
+											<a href="#">Птиц</a>
+											<ul>
+												<li><a href="#">Ястреб</a></li>
+												<li><a href="#">Торнадо</a></li>
+											</ul>
+										</li>											
+										<li><a href="#">Собак</a></li>
+										<li><a href="#">Тараканов</a></li>
+									</ul>
+								</li>							
+							</ul>
+						</div>
+						<div class="menu__column">
+							<ul class="menu__item">
+								<li>
+									<a href="#">Уничтожители</a>
+									<ul>
+										<li><a href="#">Грызунов</a></li>
+										<li><a href="#">Комаров</a></li>
+										<li><a href="#">Насекомых</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#">Электрошокеры</a>
+									<ul>
+										<li><a href="#">ОСА</a></li>
+										<li><a href="#">Парализаторы</a></li>
+										<li><a href="#">Фонари</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+						<div class="menu__column">
+							<ul class="menu__item">
+								<li>
+									<a href="#">Сад и огород</a>
+									<ul>
+										<li><a href="#">Биопрепараты для сада и огорода</a></li>
+										<li><a href="#">Капельный полив</a></li>
+										<li><a href="#">Парники</a></li>
+										<li><a href="#">Сетки от кротов</a></li>
+										<li><a href="#">Системы полива</a></li>
+										<li><a href="#">Шланги</a></li>
+									</ul>
+								</li>							
+							</ul>
+						</div>
+						<div class="menu__column">
+							<ul class="menu__item">
+								<li>
+									<a href="#">Здоровое&nbsp;питание</a>
+									<ul>
+										<li><a href="#">Зерновые напитки</a></li>
+										<li><a href="#">Макробиотические каши</a></li>
+										<li><a href="#">Оздоровительные  курсы</a></li>
+										<li><a href="#">«Народные рецепты здоровья»</a></li>
+										<li><a href="#">Для снижения веса</a></li>
+									</ul>
+								</li>
+								<li class="highlighted"><a href="#">Распродажа</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="footer__info">
+					<div class="compilation-block">
+						<h4 class="compilation-block__title">Подборки</h4>
+						<ul class="compilation-block__list">
+							<li><a href="#"><i class="far fa-bug"></i><span>Избавиться от насекомых</span></a></li>
+							<li><a href="#"><i class="far fa-shield-alt"></i><span>Защитить себя</span></a></li>
+							<li><a href="#"><i class="far fa-weight"></i><span>Сбросить вес</span></a></li>
+							<li><a href="#"><i class="far fa-paw"></i><span>Выгнать крыс раз и навсегда</span></a></li>
+							<li><a href="#"><i class="far fa-shovel"></i><span>Антикрот</span></a></li>
+						</ul>
+					</div>
+					<div class="footer__payment">
+						<span>Мы принимаем</span>
+						<img src="img/payment/visa.svg" alt="">
+						<img src="img/payment/mastercard.svg" alt="">
+						<img src="img/payment/mir.svg" alt="">
+					</div>
+				</div>
+			</div>
+			<div class="footer__bottomline">
+				<p>© 2020 Политика конфиденциальности, условия обработки персональных данных</p>
+			</div>
+		</div>
+	</footer>
+	<script src="libs/jquery/jquery-3.4.1.min.js"></script>
+	<script src="libs/slick/slick.min.js"></script>
+	<script src="libs/magnific/magnific-popup.min.js"></script>
+	<script src="libs/likely/likely.js"></script>
+	<script src="js/script.js"></script>	
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Верстка</title>
+	<meta name="viewport" content="width=device-width, shrink-to-fit=no" />	
+	<link rel="stylesheet" href="css/fonts.css" />	
+	<link rel="stylesheet" href="css/font-awesome.css" />
+	<link rel="stylesheet" href="libs/slick/slick.css" />
+	<link rel="stylesheet" href="libs/magnific/magnific-popup.min.css" />
+	<link rel="stylesheet" href="libs/likely/likely.css" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+	<header class="header">
+		<div class="wrapper">
+			
+		</div>
+	</header>
